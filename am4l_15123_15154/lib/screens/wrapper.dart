d443caa2d77fcb405dfff0fff5b1f1db101c498f
CVandermies <charlesvdm@outlook.com>
2020-04-09 15:35:30 +0200

    Wrapper ++ Screens

---
+++ am4l_15123_15154/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:am4l_15123_15154/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    //return either Home or Authenticate widget
+    return Home();
+      }
+}
