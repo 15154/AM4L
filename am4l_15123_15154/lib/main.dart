44c2a9ca67835a9b75cdfd9db1c7548a8d263d94
Mathias <15154@ecam.be>
2020-04-09 17:35:42 +0200

    provider package

--- am4l_15123_15154/lib/main.dart
+++ am4l_15123_15154/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:am4l_15123_15154/screens/wrapper.dart';
+import 'package:am4l_15123_15154/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:am4l_15123_15154/models/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,8 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-    ); //MaterialApp
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    ); // StreamProvider.value
   }
 }
--- am4l_15123_15154/lib/screens/wrapper.dart
+++ am4l_15123_15154/lib/screens/wrapper.dart
@@ -1,12 +1,17 @@
 import 'package:am4l_15123_15154/screens/authenticate/authenticate.dart';
 import 'package:am4l_15123_15154/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:am4l_15123_15154/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
     
     //return either Home or Authenticate widget
     return Authenticate();
-      }
+  }
 }
