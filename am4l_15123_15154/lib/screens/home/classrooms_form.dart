71839f43d63f0b5cf675ab8c039c59b5a8a1e4ca
Mathias <15154@ecam.be>
2020-04-17 12:21:23 +0200

    improving subscribe

--- am4l_15123_15154/lib/screens/home/classrooms_form.dart
+++ am4l_15123_15154/lib/screens/home/classrooms_form.dart
@@ -12,8 +12,11 @@ class _ClassroomsFormState extends State<ClassroomsForm> {
   final List<String> classrooms = ['1', '2', '3', '4', '5', '6'];
 
   // form values
-  String _currentName;
-  String _currentClassrooms;
+  //tring _currentName;
+  //String _currentClassrooms;
+
+  bool _firstValue = false;
+  bool _secValue = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,34 +25,144 @@ class _ClassroomsFormState extends State<ClassroomsForm> {
       child: Column(
         children: <Widget>[
           Text(
-            'Add a classroom',
+            'Subscribe to classrooms',
             style: TextStyle(fontSize: 18.0),
           ),
           SizedBox(height: 20.0),
           // dropdown
           DropdownButtonFormField(
             decoration: textInputDecoration,
-            value: _currentClassrooms ?? '1',
-            items: classrooms.map((classroom){
-              return DropdownMenuItem(
-                value: classroom,
-                child: Text('$classroom'),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => _currentClassrooms = val),
+            //value: _currentClassrooms ?? '1',
+            items: [
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+
+                      }, value: _firstValue,
+                    ),
+                    Text('1st year'),
+                  ],
+                ),
+              ),
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+                      },
+                      value: _secValue,
+                    ),
+                    Text('2nd year'),
+                  ],
+                ),
+              ),
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+                      },
+                      value: _secValue,
+                    ),
+                    Text('3rd year'),
+                  ],
+                ),
+              ),
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+                      },
+                      value: _secValue,
+                    ),
+                    Text('4th year'),
+                  ],
+                ),
+              ),
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+                      },
+                      value: _secValue,
+                    ),
+                    Text('5th year'),
+                  ],
+                ),
+              ),
+              DropdownMenuItem(
+                child: Row(
+                  children: <Widget>[
+                    Checkbox(
+                      onChanged: (bool value) {
+                      },
+                      value: _secValue,
+                    ),
+                    Text('6th year'),
+                  ],
+                ),
+              ),
+            ],
+            onChanged: (value) {
+            },
+            hint: Text('Select a '),
           ),
           RaisedButton(
             color: Colors.pink[400],
             child: Text(
-              'Add',
+              'Subscribe',
               style: TextStyle(color: Colors.white),
             ),
             onPressed: () async {
-              print(_currentName);
-              print(_currentClassrooms);
+              print(_firstValue);
+              print(_secValue);
             },
-          )
-        ],),
+          ),
+        ],
+      ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Form(
+  //     key: _formKey,
+  //     child: Column(
+  //       children: <Widget>[
+  //         Text(
+  //           'Add a classroom',
+  //           style: TextStyle(fontSize: 18.0),
+  //         ),
+  //         SizedBox(height: 20.0),
+  //         // dropdown
+  //         DropdownButtonFormField(
+  //           decoration: textInputDecoration,
+  //           value: _currentClassrooms ?? '1',
+  //           items: classrooms.map((classroom){
+  //             return DropdownMenuItem(
+  //               value: classrooms,
+  //               child: Text('$classrooms'),
+  //             );
+  //           }).toList(),
+  //           onChanged: (val) => setState(() => _currentClassrooms = val),
+  //         ),
+  //         RaisedButton(
+  //           color: Colors.pink[400],
+  //           child: Text(
+  //             'Add',
+  //             style: TextStyle(color: Colors.white),
+  //           ),
+  //           onPressed: () async {
+  //             print(_currentName);
+  //             print(_currentClassrooms);
+  //           },
+  //         )
+  //       ],
+  //      ),
+  //   );
+  // }
 }
