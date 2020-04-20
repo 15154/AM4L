30dad5020fc6255b543e2c3d47f195a6dfa6a6f7
CVandermies <charlesvdm@outlook.com>
2020-04-20 23:20:56 +0200

    ++ adList

--- am4l_15123_15154/lib/services/webservice.dart
+++ am4l_15123_15154/lib/services/webservice.dart
@@ -1,12 +1,11 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:am4l_15123_15154/models/ad.dart';
 
 class Webservice {
   Future<List<Ad>> getAllAds() async {
 
-    final response = await http.get("https://1drv.ms/u/s!Am_bfyQmoHvPqV2WMEhw_UkzGs9j?e=fT9ZNS");
+    final response = await http.get("https://shrub-berry-jujube.glitch.me/ads");
     if(response.statusCode == 200) {
 
       Iterable json = jsonDecode(response.body);
