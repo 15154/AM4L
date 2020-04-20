336b2308c62592b2ccf387f5d0c10a4350a80a8a
CVandermies <charlesvdm@outlook.com>
2020-04-20 05:48:53 +0200

    ++ adListPage

---
+++ am4l_15123_15154/lib/screens/home/adListPage.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AdListPage extends StatefulWidget {
+  @override 
+  _AdListPageState createState() => _AdListPageState(); 
+}
+
+class _AdListPageState extends State<AdListPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Provider.of<AdListViewModel>(context,listen: false).fetchAllAds();
+  }
+
+  // Widget _buildUI(AdListViewModel vm) {
+  //   if(vm.ads == null) {
+  //     return Align(child: CircularProgressIndicator());
+  //   } else if(vm.ads.isEmpty) {
+  //     return Align(child: Text("No ads found."));
+  //   } else {
+  //     return AdList(ads: vm.ads);
+  //   }
+  // }
+ 
+  @override
+  Widget build(BuildContext context) {
+
+    // final vm =  Provider.of<AdListViewModel>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+      ),
+      body: Text("Ads will be displayed here")//_buildUI(vm)
+    );
+    
+  }
+}
